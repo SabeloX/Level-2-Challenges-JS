fca5c9b4ce711d63711ffec575b671663986eef3
SabeloX <52076408+SabeloX@users.noreply.github.com>
2019-06-24 14:23:01 +0200

    Create Coffee.js

---
+++ Coffee.js
@@ -0,0 +1,12 @@
+function myCoffee(number){ /*code here*/
+    
+    if(number === 1){             // three = to also check type
+        return "French Roast";
+    } 
+    else if(number === 2){
+        return "Colombian";
+    }
+    else if(number === 3){
+        return "Kona";
+    }
+}
