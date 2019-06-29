0ff926381ac00cd75c0768df3cd5f35eaf25a64a
SabeloX <52076408+SabeloX@users.noreply.github.com>
2019-06-29 18:48:54 +0200

    Update arrayMode.js

--- arrayMode.js
+++ arrayMode.js
@@ -3,6 +3,7 @@ function arrayMode(array){ /*code here*/
     var modes = [];   // one or more modes
     var freq = [];    // number of times occuring
     var count = 0;
+    var max = 0;
     
     for(i = 0; i < array.length; i++){
         for(j = 0; j < array.length; j++){
@@ -15,5 +16,13 @@ function arrayMode(array){ /*code here*/
         count = 0;
     }
     
-    return modes[freq.indexOf(Math.max(...freq))];
+    for(k = 0; k < freq.length; k++){
+        for(m = 0; m < freq.length; m++){
+            if(freq[k] > freq[m]){
+                max = freq[k];
+            }
+        }
+    }
+    
+    return modes[freq.indexOf(max)];
 }
